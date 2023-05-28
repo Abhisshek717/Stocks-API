e5766fd37e4e6f09c799aead989007fbde44ce9d
Abhishek <46296076+Abhisshek717@users.noreply.github.com>
2023-05-28 17:19:16 +0530

    Update router.go
    added comments to each line of code

--- router/router.go
+++ router/router.go
@@ -1,36 +1,35 @@
-package router
+// Create a new router.
+router := mux.NewRouter()
 
-import(
-	
-	"log"
-	"os"
-	"database/sql"
-	"api/middleware"
-	"github.com/gorilla/mux"
-)
-
-func Router() *mux.Router {
-
-	router := mux.NewRouter()
-
-	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS stocks (stockid SERIAL PRIMARY KEY, name TEXT, company TEXT, price INT)")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-
-	router.HandleFunc("/stocks", middleware.Getstocks(db)).Methods("GET","OPTIONS")
-	router.HandleFunc("/stocks/{id}", middleware.GetStock(db)).Methods("GET","OPTIONS")
-	router.HandleFunc("/stocks", middleware.CreateStock(db)).Methods("POST","OPTIONS")
-	router.HandleFunc("/stocks/{id}", middleware.UpdateStock(db)).Methods("PUT","OPTIONS")
-	router.HandleFunc("/stocks/{id}", middleware.DeleteStock(db)).Methods("DELETE","OPTIONS")
-
-	return router
+// Open a database connection.
+db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+if err != nil {
+	// Log any errors and exit.
+	log.Fatal(err)
 }
+defer db.Close()
+
+// Create a table to store stocks.
+_, err = db.Exec("CREATE TABLE IF NOT EXISTS stocks (stockid SERIAL PRIMARY KEY, name TEXT, company TEXT, price INT)")
+if err != nil {
+	// Log any errors and exit.
+	log.Fatal(err)
+}
+
+// Handle requests to get all stocks.
+router.HandleFunc("/stocks", middleware.Getstocks(db)).Methods("GET", "OPTIONS")
+
+// Handle requests to get a specific stock by ID.
+router.HandleFunc("/stocks/{id}", middleware.GetStock(db)).Methods("GET", "OPTIONS")
+
+// Handle requests to create a new stock.
+router.HandleFunc("/stocks", middleware.CreateStock(db)).Methods("POST", "OPTIONS")
+
+// Handle requests to update an existing stock.
+router.HandleFunc("/stocks/{id}", middleware.UpdateStock(db)).Methods("PUT", "OPTIONS")
+
+// Handle requests to delete an existing stock.
+router.HandleFunc("/stocks/{id}", middleware.DeleteStock(db)).Methods("DELETE", "OPTIONS")
+
+// Return the router.
+return router
