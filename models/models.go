bc0bf4cbb75418ef56bb4ed221d8a7648a582412
Abhishek <46296076+Abhisshek717@users.noreply.github.com>
2023-05-28 17:20:23 +0530

    Update models.go
    added comments to each line

--- models/models.go
+++ models/models.go
@@ -1,8 +1,13 @@
 package models
 
+// Stock represents a stock.
 type Stock struct {
-	StockID    int64    `json:"id"`
-	Name  string `json:"name"`
+	// StockID is the unique identifier for the stock.
+	StockID int64 `json:"id"`
+	// Name is the name of the stock.
+	Name string `json:"name"`
+	// Company is the company that issued the stock.
 	Company string `json:"email"`
+	// Price is the current price of the stock.
 	Price int64 `json:"price"`
 }
